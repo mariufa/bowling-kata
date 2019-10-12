0d2daad97c584be1dfe629c045dde79d9ca7912a
Marius Fagerland <mariusfagerland@yahoo.no>
2019-10-12 18:52:36 +0200

    Add cors

--- bonus/java-api/src/main/java/com/fagerland/javaapi/BowlingController.java
+++ bonus/java-api/src/main/java/com/fagerland/javaapi/BowlingController.java
@@ -2,13 +2,11 @@ package com.fagerland.javaapi;
 
 
 import org.json.simple.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class BowlingController {
 
